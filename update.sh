630298ede540159223fc51b52f03da3d816634cb
Sun Rongyang <sun-rongyang@outlook.com>
2019-08-30 21:46:54 +0800

    Remove update script.

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Author: Rongyang Sun <sun-rongyang@outlook.com>
-# Creation Date: 2019-06-09 16:39
-# 
-# Description: GraceQ/mps2 project. Update script.
-#
-git pull
-git submodule update --remote external/gqten
