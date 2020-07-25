c781e8a78735413bb69af0ba96564452e19717cb
sun-rongyang <sun-rongyang@outlook.com>
2020-07-25 15:58:33 +0800

    Revert "Non-uniform Indices support for MPO and variational MPS algorithm; Support Noised two-site vMPS update algorithm"

--- tests/test_mps_measu.cc
+++ tests/test_mps_measu.cc
@@ -9,7 +9,7 @@
 #include "gqten/gqten.h"
 
 #include "gtest/gtest.h"
-#include <stdlib.h>
+
 
 using namespace gqmps2;
 using namespace gqten;
@@ -76,18 +76,6 @@ void RunTestMeasureOneSiteOpCase(
   }
 }
 
-///support for non-uniform index
-template <typename MpsType, typename TenElemType>
-void RunTestMeasureOneSiteOpCase(
-  MpsType &mps,
-  const GQTensor<TenElemType> &op, const std::vector<long> site_set,
-  const std::vector<TenElemType> &res) {
-  auto measu_res = MeasureOneSiteOp(mps, op,site_set, "op1");
-  assert(measu_res.size() == res.size());
-  for (size_t i = 0; i < res.size(); ++i) {
-    ExpectDoubleEq(measu_res[i].avg, res[i]);
-  }
-}
 
 TEST_F(TestMpsMeasurement, TestMeasureOneSiteOp) {
   // Double case 1
@@ -191,89 +179,3 @@ TEST_F(TestMpsMeasurement, TestMeasureTwoSiteOp) {
       zmps_for_measu2, {zntot, zntot}, zid, zid, sites_set, zres2);
   MpsFree(zmps2);
 }
-
-
-struct TestNonUniformMpsMeasurement : public testing::Test {
-  long N = 4; //unit cell number, A-B-A-B-A-B-A-B
-
-  QN qn0 = QN({QNNameVal("N", 0)});
-  Index pb_outA = Index({QNSector(QN({QNNameVal("N", 0)}), 1),
-                        QNSector(QN({QNNameVal("N", 1)}), 1)}, OUT);
-  Index pb_inA = InverseIndex(pb_outA);
-  Index pb_outB = Index({QNSector(QN({QNNameVal("N", 0)}), 2)}, OUT);
-  Index pb_inB = InverseIndex(pb_outB);
-
-  DGQTensor dntotA = DGQTensor({pb_inA, pb_outA});
-  DGQTensor didA = DGQTensor({pb_inA, pb_outA});
-  DGQTensor didB = DGQTensor({pb_inB, pb_outB});
-  DTenPtrVec dmps = DTenPtrVec(2*N);
-  std::vector<Index> pb_out_set = std::vector<Index>(2*N);
-
-  std::vector<long> stat_labs1 = std::vector<long>(2*N,1);
-  std::vector<long> A_sublattice=std::vector<long>(N);
-
-  void SetUp(void) {
-    dntotA({0, 0}) = 0;
-    dntotA({1, 1}) = 1;
-    didA({0, 0}) = 1;
-    didA({1, 1}) = 1;
-
-    didB({0, 0}) = 1;
-    didB({1, 1}) = 1;
-    srand((unsigned)time(NULL));
-    for (long i = 0; i < 2*N; ++i) {
-      if(i%2==0) pb_out_set[i]=pb_outA;
-      else pb_out_set[i]=pb_outB;
-    }
-    for (long i=0;i<N;++i){
-      A_sublattice[i]=2*i;
-      stat_labs1[2*i]= rand()%2;
-    }
-  }
-};
-
-
-TEST_F(TestNonUniformMpsMeasurement, TestNoUniformMeasureOneSiteOp) {
-  std::vector<GQTEN_Double> dres1(N);
-  for (long i = 0; i < N; ++i) { dres1[i]=stat_labs1[2*i]; }
-  auto dmps1 = dmps;
-  DirectStateInitMps(dmps1, stat_labs1, pb_out_set, qn0);
-  auto dmps_for_measu1 = MPS<DGQTensor>(dmps1, -1);
-  RunTestMeasureOneSiteOpCase(dmps_for_measu1, dntotA,A_sublattice, dres1);
-  MpsFree(dmps1);
-}
-
-///No insertion operator case, can used for uniform or non-uniform indices
-template <typename MpsType, typename TenElemType>
-void RunTestMeasureTwoSiteOpCase(
-  MpsType &mps,
-  const std::vector<GQTensor<TenElemType>> &phys_ops,
-  const std::vector<std::vector<long>> &sites_set,
-  const std::vector<TenElemType> &res) {
-  auto measu_res = MeasureTwoSiteOp(
-    mps, phys_ops, sites_set, "op1op2");
-  assert(measu_res.size() == res.size());
-  for (size_t i = 0; i < res.size(); ++i) {
-    ExpectDoubleEq(measu_res[i].avg, res[i]);
-  }
-}
-
-TEST_F(TestNonUniformMpsMeasurement, TestNoUniformMeasureTwoSiteOp) {
-std::vector<std::vector<long>> sites_set = {
-  {0, 2}, {0, 4},{2, 4}
-};
-// Double case 1
-std::vector<GQTEN_Double> dres1(sites_set.size());
-for(int i = 0;i<sites_set.size();i++){
-  long site1 = sites_set[i][0];
-  long site2 = sites_set[i][1];
-  dres1[i]=stat_labs1[site1]*stat_labs1[site2];
-}
-auto dmps1 = dmps;
-DirectStateInitMps(dmps1, stat_labs1, pb_out_set, qn0);
-auto dmps_for_measu1 = MPS<DGQTensor>(dmps1, -1);
-RunTestMeasureTwoSiteOpCase(
-  dmps_for_measu1, {dntotA, dntotA}, sites_set, dres1);
-MpsFree(dmps1);
-}
-
