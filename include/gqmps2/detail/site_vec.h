7cd3c9d1012bdf92fc1ff84801058b28a77cc0f2
Sun Rongyang <sun-rongyang@outlook.com>
2020-07-29 19:28:43 +0800

    Implemnt SiteVec draft.

---
+++ include/gqmps2/detail/site_vec.h
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+/*
+* Author: Rongyang Sun <sun-rongyang@outlook.com>
+* Creation Date: 2020-07-29 14:29
+* 
+* Description: GraceQ/MPS2 project. One-dimensional vector representation of the local Hilbert spaces of the system.
+*/
+
+/** @file site_vec.h
+ *  @brief One-dimensional vector representation of the local Hilbert spaces of the system.
+ */
+#ifndef GQMPS2_DETAIL_SITE_VEC_H
+#define GQMPS2_DETAIL_SITE_VEC_H
+
+
+#include "gqten/gqten.h"  // IndexVec
+
+#include <assert.h>       // assert
+
+
+#ifdef Release
+  #define NDEBUG
+#endif
+
+
+namespace gqmps2 {
+using namespace gqten;
+
+
+// Helpers
+inline
+Index SetIndexDirOut(const Index &idx) {
+  if (idx.dir == OUT) {
+    return idx;
+  } else {
+    return InverseIndex(idx);
+  }
+}
+
+
+/** Vector of the local Hilbert spaces of the system.
+ */
+class SiteVec {
+public:
+  /// Create a system with N identical sites.
+  SiteVec(
+      const size_t N,                     ///< Total number of sites of the system.
+      const Index &local_hilbert_space    ///< Local (on-site) Hilbert space represented by a Index.
+  ) {
+    size = N;
+    sites = IndexVec(N, SetIndexDirOut(local_hilbert_space));
+  }
+
+  /// Create a sites vector using a vector of arbitrary Indexes.
+  SiteVec(
+      const IndexVec &local_hilbert_spaces    ///< Local (on-site) Hilbert spaces on the each site.
+  ) {
+    size = local_hilbert_spaces.size(); 
+    sites.reserve(size);
+    for (size_t i = 0; i < size; ++i) {
+      sites.emplace_back(SetIndexDirOut(local_hilbert_spaces[i]));
+    }
+  }
+
+  size_t size;        ///< The size of the SiteVec, i.e. the size of the system.
+  IndexVec sites;     ///< Local Hilbert spaces represented by a vector of Index with OUT direction.
+};
+} /* gqmps2 */ 
+#endif /* ifndef GQMPS2_DETAIL_SITE_VEC_H */
---
+++ tests/test_site_vec.cc
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+* Author: Rongyang Sun <sun-rongyang@outlook.com>
+* Creation Date: 2020-07-29 16:48
+* 
+* Description: GraceQ/MPS2 project. Unittests for SiteVec .
+*/
+#include "gqmps2/detail/site_vec.h"
+#include "gqten/gqten.h"
+
+#include "gtest/gtest.h"
+
+
+using namespace gqmps2;
+using namespace gqten;
+
+
+void RunTestSiteVecBasicFeatures(
+    const size_t N,
+    const Index &local_hilbert_space
+) {
+  SiteVec site_vec(N, local_hilbert_space);
+  EXPECT_EQ(site_vec.size, N);
+  Index site;
+  if (local_hilbert_space.dir == OUT) {
+    site = local_hilbert_space;
+  } else {
+    site = InverseIndex(local_hilbert_space);
+  }
+    EXPECT_EQ(site_vec.sites, IndexVec(N, site));
+}
+
+
+void RunTestSiteVecBasicFeatures(const IndexVec &local_hilbert_spaces) {
+  SiteVec site_vec(local_hilbert_spaces);
+  EXPECT_EQ(site_vec.size, local_hilbert_spaces.size());
+  for (size_t i = 0; i < site_vec.size; ++i) {
+    if (local_hilbert_spaces[i].dir == OUT) {
+      EXPECT_EQ(site_vec.sites[i], local_hilbert_spaces[i]);
+    } else {
+      EXPECT_EQ(site_vec.sites[i], InverseIndex(local_hilbert_spaces[i]));
+    }
+  }
+}
+
+
+TEST(TestSiteVec, TestBasicFeatures) {
+  Index pb_out1 = Index({
+                      QNSector(QN({QNNameVal("N", 0)}), 1),
+                      QNSector(QN({QNNameVal("N", 1)}), 1)},
+                      OUT
+                  );
+  Index pb_in1 = InverseIndex(pb_out1);
+  Index pb_out2 = Index({
+                      QNSector(QN({QNNameVal("N", 1)}), 3)},
+                      OUT
+                  );
+  Index pb_in2 = InverseIndex(pb_out2);
+  RunTestSiteVecBasicFeatures(1, pb_out1);
+  RunTestSiteVecBasicFeatures(1, pb_in1);
+  RunTestSiteVecBasicFeatures(3, pb_out1);
+  RunTestSiteVecBasicFeatures(3, pb_in1);
+
+  RunTestSiteVecBasicFeatures({pb_out1});
+  RunTestSiteVecBasicFeatures({pb_out2, pb_out2});
+  RunTestSiteVecBasicFeatures({pb_out1, pb_out2, pb_out1});
+  RunTestSiteVecBasicFeatures({pb_in2, pb_out1, pb_out1, pb_in1, pb_out2});
+}
