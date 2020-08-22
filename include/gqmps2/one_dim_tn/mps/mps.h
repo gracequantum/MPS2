99bd097439a057b663d8f20689d65fc680c56c87
Sun Rongyang <sun-rongyang@outlook.com>
2020-08-22 17:01:48 +0800

    feat:A new MPS class based on TenVec

--- include/gqmps2/one_dim_tn/mps/mps.h
+++ include/gqmps2/one_dim_tn/mps/mps.h
@@ -15,20 +15,241 @@
 #define GQMPS2_ONE_DIM_TN_MPS_MPS_H
 
 
+#include "gqmps2/one_dim_tn/framework/ten_vec.h"    // TenVec
+#include "gqten/gqten.h"    // Svd, Contract
+
 #include <vector>     // vector
+
+#include <assert.h>     // assert
+
+
+#ifdef Release
+  #define NDEBUG
+#endif
 
 
 namespace gqmps2 {
+using namespace gqten;
 
 
-template <typename TenType>
-struct MPS {
-  MPS(std::vector<TenType *> &tens, const long center) :
-      tens(tens), center(center), N(tens.size()) {}
-  
-  std::vector<TenType *> &tens; 
-  long center;
-  std::size_t N;
+const int kUncentralizedCenterIdx = -1;
+
+
+/// Canonical type of a MPS local tensor.
+enum MPSTenCanoType {
+  NONE,   ///< Not a canonical MPS tensor.
+  LEFT,   ///< Left canonical MPS tensor.
+  RIGHT   ///< Right canonical MPS tensor.
 };
-} /* gqmps2 */ 
+
+
+/**
+The matrix product state (MPS) class.
+
+@tparam ElemT Type of the MPS local tensors.
+*/
+template <typename ElemT>
+class MPS : public TenVec<ElemT> {
+public:
+  /**
+  Create a empty MPS using its size.
+
+  @param size The size of the MPS.
+  */
+  MPS(
+      const size_t size
+  ) : TenVec<ElemT>(size),
+      center_(kUncentralizedCenterIdx),
+      tens_cano_type_(size, MPSTenCanoType::NONE) {}
+
+  // MPS local tensor access.
+  /**
+  Access to local tensor. Set canonical type to NONE and MPS to uncentralized.
+
+  @param idx Index of the MPS local tensor.
+  */
+  ElemT &operator[](const size_t idx) {
+    tens_cano_type_[idx] = MPSTenCanoType::NONE;
+    center_ = kUncentralizedCenterIdx;
+    return DuoVector<ElemT>::operator[](idx);
+  }
+
+  /**
+  Read-only access to local tensor.
+
+  @param idx Index of the MPS local tensor.
+  */
+  const ElemT &operator[](const size_t idx) const {
+    return DuoVector<ElemT>::operator[](idx); 
+  }
+
+  /**
+  Access to the pointer to local tensor. Set canonical type to NONE and MPS to
+  uncentralized.
+
+  @param idx Index of the MPS local tensor.
+  */
+  ElemT * &operator()(const size_t idx) {
+    tens_cano_type_[idx] = MPSTenCanoType::NONE;
+    center_ = kUncentralizedCenterIdx; 
+    return DuoVector<ElemT>::operator()(idx);
+  }
+
+  /**
+  Read-only access to the pointer to local tensor.
+
+  @param idx Index of the MPS local tensor.
+  */
+  const ElemT *operator()(const size_t idx) const {
+    return DuoVector<ElemT>::operator()(idx); 
+  }
+
+  // MPS global operations.
+  void Centralize(const int);
+
+  // Properties getter.
+  /**
+  Get the center of the MPS.
+  */
+  int GetCenter(void) const { return center_; }
+
+  /**
+  Get the canonical type of all of the MPS local tensors.
+  */
+  std::vector<MPSTenCanoType> GetTensCanoType(void) const {
+    return tens_cano_type_;
+  }
+
+  /**
+  Get the canonical type of a MPS local tensor.
+
+  @param idx Index of the MPS local tensor.
+  */
+  MPSTenCanoType GetTenCanoType(const size_t idx) const {
+    return tens_cano_type_[idx];
+  }
+  
+private:
+  int center_;
+  std::vector<MPSTenCanoType> tens_cano_type_;
+
+  void LeftCanonicalize_(const size_t);
+  void LeftCanonicalizeTen_(const size_t);
+  void RightCanonicalize_(const size_t);
+  void RightCanonicalizeTen_(const size_t);
+};
+
+
+/**
+Centralize the MPS.
+
+@param target_center The new center of the MPS.
+*/
+template <typename ElemT>
+void MPS<ElemT>::Centralize(const int target_center) {
+  assert(target_center >= 0);
+  auto mps_tail_idx = this->size() - 1; 
+  if (target_center != 0) { LeftCanonicalize_(target_center - 1); }
+  if (target_center != mps_tail_idx) {
+    RightCanonicalize_(target_center + 1);
+  }
+  center_ = target_center;
+}
+
+
+template <typename ElemT>
+void MPS<ElemT>::LeftCanonicalize_(const size_t stop_idx) {
+  size_t start_idx;
+  for (size_t i = 0; i <= stop_idx; ++i) {
+    start_idx = i;
+    if (tens_cano_type_[i] != MPSTenCanoType::LEFT) { break; }
+    if (i == stop_idx) { return; }    // All related tensors are left canonical, do nothing.
+  }
+  for (size_t i = start_idx; i <= stop_idx; ++i) { LeftCanonicalizeTen_(i); }
+}
+
+
+template <typename ElemT>
+void MPS<ElemT>::LeftCanonicalizeTen_(const size_t site_idx) {
+  assert(site_idx < this->size() - 1);
+  long ldims, rdims;
+  if (site_idx == 0) {
+    ldims = 1;
+    rdims = 1;
+  } else {
+    ldims = 2;
+    rdims = 1;
+  }
+  auto svd_res = Svd(
+      (*this)[site_idx],
+      ldims, rdims,
+      Div((*this)[site_idx]), Div((*this)[site_idx+1])
+  );
+  delete (*this)(site_idx);
+  (*this)(site_idx) = svd_res.u;
+  auto temp_ten = Contract(*svd_res.s, *svd_res.v, {{1}, {0}});
+  delete svd_res.s;
+  delete svd_res.v;
+  auto next_ten = Contract(*temp_ten, (*this)[site_idx+1], {{1}, {0}});
+  delete temp_ten;
+  delete (*this)(site_idx + 1);
+  (*this)(site_idx + 1) = next_ten;
+
+  tens_cano_type_[site_idx] = MPSTenCanoType::LEFT;
+  tens_cano_type_[site_idx + 1] = MPSTenCanoType::NONE;
+}
+
+
+template <typename ElemT>
+void MPS<ElemT>::RightCanonicalize_(const size_t stop_idx) {
+  auto mps_tail_idx = this->size() - 1;
+  size_t start_idx;
+  for (size_t i = mps_tail_idx; i >= stop_idx; --i) {
+    start_idx = i;
+    if (tens_cano_type_[i] != MPSTenCanoType::RIGHT) { break; }
+    if (i == stop_idx) { return; }    // All related tensors are right canonical, do nothing.
+  }
+  for (size_t i = start_idx; i >= stop_idx; --i) { RightCanonicalizeTen_(i); }
+}
+
+
+template <typename ElemT>
+void MPS<ElemT>::RightCanonicalizeTen_(const size_t site_idx) {
+  assert(site_idx > 0);
+  long ldims, rdims;
+  if (site_idx == this->size() - 1) {
+    ldims = 1;
+    rdims = 1;
+  } else {
+    ldims = 1;
+    rdims = 2;
+  }
+  auto svd_res = Svd(
+      (*this)[site_idx],
+      ldims, rdims,
+      Div((*this)[site_idx - 1]), Div((*this)[site_idx])
+  );
+  delete (*this)(site_idx);
+  (*this)(site_idx) = svd_res.v;
+  auto temp_ten = Contract(*svd_res.u, *svd_res.s, {{1}, {0}});
+  delete svd_res.u;
+  delete svd_res.s;
+  std::vector<long> ta_ctrct_axes;
+  if ((site_idx - 1) == 0) {
+    ta_ctrct_axes = {1};
+  } else {
+    ta_ctrct_axes = {2};
+  }
+  auto prev_ten = Contract(
+                      (*this)[site_idx - 1], *temp_ten,
+                      {ta_ctrct_axes, {0}}
+                  );
+  delete temp_ten;
+  delete (*this)(site_idx - 1);
+  (*this)(site_idx - 1) = prev_ten;
+
+  tens_cano_type_[site_idx] = MPSTenCanoType::RIGHT;
+  tens_cano_type_[site_idx - 1] = MPSTenCanoType::NONE;
+}
+} /* gqmps2 */
 #endif /* ifndef GQMPS2_ONE_DIM_TN_MPS_MPS_H */
---
+++ tests/test_one_dim_tn/test_mps.cc
@@ -0,0 +1,171 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+/*
+* Author: Rongyang Sun <sun-rongyang@outlook.com>
+* Creation Date: 2020-08-21 09:13
+* 
+* Description: GraceQ/MPS2 project. Unittests for MPS .
+*/
+#include "gqmps2/one_dim_tn/mps/mps.h"
+#include "gqten/gqten.h"
+
+#include "gtest/gtest.h"
+
+using namespace gqmps2;
+using namespace gqten;
+
+using Tensor = DGQTensor;
+
+
+struct TestMPS : public testing::Test {
+  QN qn0 = QN({QNNameVal("N", 0)});
+  QN qn1 = QN({QNNameVal("N", 1)});
+  QN qn2 = QN({QNNameVal("N", 2)});
+  Index pb_out = Index({QNSector(qn0, 1), QNSector(qn1, 1)}, OUT);
+  Index vb01_out = Index({QNSector(qn0, 3), QNSector(qn1, 3)}, OUT);
+  Index vb01_in = InverseIndex(vb01_out);
+  Index vb012_out = Index(
+                        {QNSector(qn0, 3), QNSector(qn1, 3), QNSector(qn2, 3)},
+                        OUT
+                    );
+  Index vb012_in = InverseIndex(vb012_out);
+  Tensor t0 = Tensor({pb_out, vb01_out});
+  Tensor t1 = Tensor({vb01_in, pb_out, vb012_out});
+  Tensor t2 = Tensor({vb012_in, pb_out, vb012_out}); 
+  Tensor t3 = Tensor({vb012_in, pb_out, vb01_out});
+  Tensor t4 = Tensor({vb01_in, pb_out});
+
+  MPS<Tensor> mps = MPS<Tensor>(5);
+
+  void SetUp(void) {
+    t0.Random(qn0); 
+    t1.Random(qn0); 
+    t2.Random(qn0); 
+    t3.Random(qn0); 
+    t4.Random(qn0); 
+    mps[0] = t0;
+    mps[1] = t1;
+    mps[2] = t2;
+    mps[3] = t3;
+    mps[4] = t4;
+  }
+};
+
+
+// Helpers
+template <typename TenT>
+void CheckIsIdTen(const TenT &t) {
+  auto shape = t.shape;
+  EXPECT_EQ(shape.size(), 2);
+  EXPECT_EQ(shape[0], shape[1]);
+  for (long i = 0; i < shape[0]; ++i) {
+    EXPECT_DOUBLE_EQ(t.Elem({i, i}), 1.0);
+  }
+}
+
+
+template <typename ElemT>
+void CheckMPSTenCanonical(
+    const MPS<ElemT> &mps,
+    const size_t i,
+    const int center) {
+  std::vector<std::vector<long>> ctrct_leg_idxs;
+  if (i < center) {
+    if (i == 0) {
+      ctrct_leg_idxs = {{0}, {0}};
+    } else {
+      ctrct_leg_idxs = {{0, 1}, {0, 1}};
+    }
+  } else if (i > center) {
+    if (i == mps.size() - 1) {
+      ctrct_leg_idxs = {{1}, {1}};
+    } else {
+      ctrct_leg_idxs = {{1, 2}, {1, 2}};
+    }
+  }
+
+  Tensor res;
+  auto ten = mps[i];
+  auto ten_dag = Dag(ten);
+  Contract(&ten, &ten_dag, ctrct_leg_idxs, &res);
+  CheckIsIdTen(res);
+}
+
+
+template <typename ElemT>
+void CheckMPSCenter(const MPS<ElemT> &mps, const int center) {
+  EXPECT_EQ(mps.GetCenter(), center);
+  
+  auto mps_size = mps.size();
+  auto tens_cano_type = mps.GetTensCanoType();
+  for (size_t i = 0; i < mps_size; ++i) {
+    if (i < center) {
+      EXPECT_EQ(tens_cano_type[i], MPSTenCanoType::LEFT);
+      CheckMPSTenCanonical(mps, i, center);
+    }
+    if (i > center) {
+      EXPECT_EQ(tens_cano_type[i], MPSTenCanoType::RIGHT);
+      CheckMPSTenCanonical(mps, i, center);
+    }
+    if (i == center) {
+      EXPECT_EQ(tens_cano_type[i], MPSTenCanoType::NONE);
+    }
+  }
+}
+
+
+template <typename ElemT>
+void RunTestMPSCentralizeCase(MPS<ElemT> &mps, const int center) {
+    mps.Centralize(center);
+    CheckMPSCenter(mps, center);
+}
+
+
+template <typename ElemT>
+void RunTestMPSCentralizeCase(MPS<ElemT> &mps) {
+  for (int i = 0; i < mps.size(); ++i) {
+    RunTestMPSCentralizeCase(mps, i);
+  }
+}
+
+
+TEST_F(TestMPS, TestCentralize) {
+  RunTestMPSCentralizeCase(mps, 0);
+  RunTestMPSCentralizeCase(mps, 2);
+  RunTestMPSCentralizeCase(mps, 4);
+  RunTestMPSCentralizeCase(mps);
+
+  mps[0].Random(qn0);
+  RunTestMPSCentralizeCase(mps, 0);
+  RunTestMPSCentralizeCase(mps, 2);
+  RunTestMPSCentralizeCase(mps, 4);
+  RunTestMPSCentralizeCase(mps);
+
+  mps[1].Random(qn0);
+  mps[2].Random(qn0);
+  mps[4].Random(qn0);
+  RunTestMPSCentralizeCase(mps, 0);
+  RunTestMPSCentralizeCase(mps, 2);
+  RunTestMPSCentralizeCase(mps, 4);
+  RunTestMPSCentralizeCase(mps);
+}
+
+
+TEST_F(TestMPS, TestElemAccess) {
+  mps.Centralize(2);
+
+  const MPS<Tensor> &crmps = mps;
+  Tensor ten = crmps[1];
+  EXPECT_EQ(mps.GetTenCanoType(1), MPSTenCanoType::LEFT);
+  ten = mps[1];
+  EXPECT_EQ(mps.GetTenCanoType(1), MPSTenCanoType::NONE);
+  EXPECT_EQ(crmps.GetTenCanoType(1), MPSTenCanoType::NONE);
+
+  const MPS<Tensor> *cpmps = &mps;
+  ten = (*cpmps)[3];
+  EXPECT_EQ(mps.GetTenCanoType(3), MPSTenCanoType::RIGHT);
+  MPS<Tensor> *pmps = &mps;
+  ten = (*pmps)[3];
+  EXPECT_EQ(mps.GetTenCanoType(3), MPSTenCanoType::NONE);
+  EXPECT_EQ(crmps.GetTenCanoType(3), MPSTenCanoType::NONE);
+}
