2cee28fa0e39a261303935f4b1b067986ad335b6
Rong-Yang Sun <sun-rongyang@outlook.com>
2020-12-17 17:41:14 +0800

    refactor(MPS related): Add a new header file to include all staff for using MPS

---
+++ include/gqmps2/one_dim_tn/mps_all.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+* Author: Rongyang Sun <sun-rongyang@outlook.com>
+* Creation Date: 2020-12-17 14:19
+*
+* Description: GraceQ/MPS2 project. All staff for using matrix product state (MPS).
+*/
+
+/**
+@file mps_all.h
+@brief All staff for using matrix product state (MPS).
+*/
+#ifndef GQMPS2_ONE_DIM_TN_MPS_ALL_H
+#define GQMPS2_ONE_DIM_TN_MPS_ALL_H
+
+
+#include "gqmps2/one_dim_tn/mps/mps.h"
+#include "gqmps2/one_dim_tn/mps/mps_init.h"
+#include "gqmps2/one_dim_tn/mps/mps_measu.h"
+
+
+#endif /* ifndef GQMPS2_ONE_DIM_TN_MPS_ALL_H */
